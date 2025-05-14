1481333db7d7ecf8ce7b7a5dc6e1f953211f3324
yungbote <yungbote@gmail.com>
2025-05-14 11:32:17 -0400

    csdac

--- src/pages/Roles.tsx
+++ src/pages/Roles.tsx
@@ -1,4 +1,4 @@
-import { RolesManagementContent } from '@/components/common/dialogs/RolesManagementContent'
+import { RolesManagementContent } from '@/components/common/dialogs/content/RolesManagementContent'
 
 export default function RolesPage() {
   return <RolesManagementContent />
