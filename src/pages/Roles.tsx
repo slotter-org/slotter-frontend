82fad4bbada6e24d1e005443ebeb866b129fb005
yungbote <yungbote@gmail.com>
2025-05-16 00:18:05 -0400

    csacac

--- src/pages/Roles.tsx
+++ src/pages/Roles.tsx
@@ -22,9 +22,7 @@ export function RolesPage() {
           />
         </div>
       </div>
-      <div className="flex-1 min-h-0">
-        <RolesManagementContent />
-      </div>
+      <RolesManagementContent />
     </div>
   ) 
 }
