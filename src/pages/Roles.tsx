778ef155cf000d7699413f879b726dea7b4ebf73
yungbote <yungbote@gmail.com>
2025-05-16 01:26:04 -0400

    csdac

--- src/pages/Roles.tsx
+++ src/pages/Roles.tsx
@@ -6,7 +6,7 @@ import { useMyCompany } from '@/contexts/MyCompanyProvider'
 export function RolesPage() {
   const { fetchMyRoles } = useMyCompany()
   return (
-    <div className="flex flex-col flex-1 h-full p-4">
+    <div className="flex flex-col h-full p-4">
       <div className="flex justify-between items-center mb-4 w-full">
         <h1 className="text-3xl font-bold">Role & Permission Management</h1>
         <div className="ml-auto">
