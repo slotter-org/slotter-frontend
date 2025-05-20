ca18cccc39899dd6b09266fb6c17c31a9ebbfbb9
yungbote <yungbote@gmail.com>
2025-05-20 10:11:29 -0400

    csdac

--- src/components/common/cards/filter/RoleCardFilter.tsx
+++ src/components/common/cards/filter/RoleCardFilter.tsx
@@ -112,7 +112,7 @@ export function RoleCardFilter({
         setShowGridButton(true);
       } else {
         setRolesCompact(false);
-        setRoleButton(false);
+        setRoleButtonTitles(false);
         setShowGridButton(true);
       }
     }
